b9d533c8e5f9f8ecb1658b440b266016336acffa
Matt Aimonetti <mattaimonetti@gmail.com>
2013-06-07 23:50:41 +0200

    fixed formatting

--- aws_config/aws_config.go
+++ aws_config/aws_config.go
@@ -1,42 +1,41 @@
-/* 
+/*
 *  This package is a Revel module that extracts config values fron app.conf
 * when the app boots. While this package exports the extracted values, it also
 * makes available variables of types defined by the popular launchpad.net/goamz/aws package.
-* 
-* Using these variables, you can start using S3, EC2 etc.. packages without having to 
+*
+* Using these variables, you can start using S3, EC2 etc.. packages without having to
 * to worry about extracting the AWS config settings.
 *
-*/
+* revel.OnAppStart(func() { awsConfig.Setup() })
+ */
 package awsConfig
 
 import (
-  "github.com/robfig/revel"
-  "launchpad.net/goamz/aws"
-  "fmt"
+	"fmt"
+	"github.com/robfig/revel"
+	"launchpad.net/goamz/aws"
 )
 
 var (
-	AccessKey  string
-  SecretKey  string
-  Region aws.Region
-  Auth aws.Auth
+	AccessKey string
+	SecretKey string
+	Region    aws.Region
+	Auth      aws.Auth
 )
 
-func init() {
-  revel.OnAppStart(func() {
+func Setup() {
 
-    configRequired := func(key string) string {
-      value, found := revel.Config.String(key); 
-      if !found {
-        revel.ERROR.Fatal(fmt.Sprintf("Configuration for %s missing in app.conf.", key))
-      }
-      return value
-    }
+	configRequired := func(key string) string {
+		value, found := revel.Config.String(key)
+		if !found {
+			revel.ERROR.Fatal(fmt.Sprintf("Configuration for %s missing in app.conf.", key))
+		}
+		return value
+	}
 
-    AccessKey = configRequired("aws.access_key")
-    SecretKey = configRequired("aws.secret_key")
-    Region = aws.Regions[configRequired("aws.region")]
-    Auth = aws.Auth{AccessKey: AccessKey, SecretKey: SecretKey}
-    revel.TRACE.Println("AwsConfig setup")
-  })
+	AccessKey = configRequired("aws.access_key")
+	SecretKey = configRequired("aws.secret_key")
+	Region = aws.Regions[configRequired("aws.region")]
+	Auth = aws.Auth{AccessKey: AccessKey, SecretKey: SecretKey}
+	revel.TRACE.Println("AwsConfig setup")
 }
